0749fbe7588ed1d713beccf340f8942d38ba3b78
= <=>
2026-04-29 18:13:51 -0400

    Made changes to command.sh - 30/04/2026

---
+++ command.sh
@@ -0,0 +1,5 @@
+docker network create my-network
+
+docker run -d --name mysql-db --network my-network -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=cloud -p 3306:3306 mysql:8.4
+
+docker run -d --name python-demo-flask-app --network my-network -p 5000:5000 -e DB_HOST=mysql-db -e DB_USER=root -e DB_PASSWORD=root -e DB_NAME=cloud liteshz/python-demo-flask-app:v5
