b34eb441f61895c40c2d7c4f68f94c57d8e4af69
= <=>
2026-04-29 17:13:25 -0400

    Added fixess - 30/06/2026

--- commandds.sh
+++ commandds.sh
@@ -2,4 +2,4 @@ docker network create my-network
 
 docker run -d --name mysql-db --network my-network -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=cloud -p 3306:3306 mysql:8.4
 
-docker run -d --name python-demo-flask-app --network my-network -p 5000:5000 --link mysql-db:mysql -e DB_HOST=mysql -e DB_USER=root -e DB_PASSWORD=root -e DB_NAME=cloud liteshz/python-demo-flask-app
+docker run -d --name python-demo-flask-app --network my-network -p 5000:5000 --link mysql-db:mysql -e DB_HOST=mysql -e DB_USER=root -e DB_PASSWORD=root -e DB_NAME=cloud liteshz/python-demo-flask-app:v2
